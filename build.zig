12d0f67bdfa0f14d8a34d93f29df8fe1188839b7
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-04-09 22:01:22 +0530

    Booting with qemu

--- build.zig
+++ build.zig
@@ -32,20 +32,8 @@ pub fn build(b: *std.Build) void {
     const kernel_step = b.step("kernel", "Build the kernel");
     kernel_step.dependOn(&kernel.step);
 
-    //    const iso_dir = b.fmt("{s}/iso_root", .{b.cache_root});
-    const iso_dir = std.mem.concat(b.allocator, u8, &[_][]const u8{ b.cache_root.path.?, "/iso_root" }) catch unreachable;
-    const kernel_path = std.mem.concat(b.allocator, u8, &[_][]const u8{ b.install_path, "/bin/", kernel.out_filename }) catch unreachable;
-    const iso_path = b.fmt("{s}/disk.iso", .{b.exe_dir});
-
-    const iso_cmd_str = &[_][]const u8{ "/bin/sh", "-c", std.mem.concat(b.allocator, u8, &[_][]const u8{ "mkdir -p ", iso_dir, " && ", "cp ", kernel_path, " ", iso_dir, " && ", "cp src/grub.cfg ", iso_dir, " && ", "grub-mkrescue -o ", iso_path, " ", iso_dir }) catch unreachable };
-    const iso_cmd = b.addSystemCommand(iso_cmd_str);
-    iso_cmd.step.dependOn(kernel_step);
-
-    const iso_step = b.step("iso", "Build Iso");
-    iso_step.dependOn(&iso_cmd.step);
-    b.default_step.dependOn(iso_step);
-
-    const run_cmd_str = [_][]const u8{ "qemu-system-i386", "-cdrom", iso_path, "-debugcon", "stdio", "-vga", "virtio", "-m", "4G", "-machine", "q35", "-no-reboot", "-no-shutdown" };
+    const kernel_path = b.fmt("{s}/{s}", .{ b.exe_dir, kernel.out_filename });
+    const run_cmd_str = [_][]const u8{ "qemu-system-x86_64", "-kernel", kernel_path };
 
     const run_cmd = b.addSystemCommand(&run_cmd_str);
     run_cmd.step.dependOn(b.getInstallStep());
